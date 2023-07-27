e63da0c6457d5b537a907f540e2b2833df330d32
Nataly Galushka <37546130+hellared@users.noreply.github.com>
2023-07-27 12:39:34 +0300

    Add authentication

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import AuthForm from "./AuthForm";
-import mutation from "../mutations/Login";
 import { graphql } from "react-apollo";
+import mutation from "../mutations/Signup";
 import query from "../queries/CurrentUser";
 import { hashHistory } from "react-router";
 
-class LoginForm extends Component {
+class SignupForm extends Component {
   constructor(props) {
     super(props);
 
@@ -13,9 +13,6 @@ class LoginForm extends Component {
   }
 
   componentWillUpdate(nextProps) {
-    // this.props //the old, current set of props
-    // nextProps // the next set of props that will be in place
-    // when the component rerenders
     if (!this.props.data.user && nextProps.data.user) {
       hashHistory.push("/dashboard");
     }
@@ -36,7 +33,7 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        <h3>Login</h3>
+        <h3>Sign Up</h3>
         <AuthForm
           errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)}
@@ -46,4 +43,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(query)(graphql(mutation)(LoginForm));
+export default graphql(query)(graphql(mutation)(SignupForm));
